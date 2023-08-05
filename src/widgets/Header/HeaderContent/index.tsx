53a2b029786e35fd4cbdfbd06c5b76165fb790f3
Raman Krytski <rkeydevelop@gmail.com>
2023-08-05 09:26:54 +0200

    vars: variables.sass implemented

---
+++ src/widgets/Header/HeaderContent/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import './style.css'
+
+const HeaderContent = () => {
+	return <div className="header-content">
+		<div className="header-content-heading">
+			<div></div>
+			<h2 className="header-content-h2">Hi there :)</h2>
+		</div>
+		<div className="header-content-container">
+			<div></div>
+			<div className="header-content-text-container">
+				<p>Hey there!</p>
+
+				<p>
+					My name is Raman Krytski, and I'm 27 years old. I really like programming,
+					so I went to study it at Minsk College of Business and Law.
+					I worked on cool projects and faced challenges during my time there,
+					which made me even more interested in programming.
+				</p>
+
+				<p>
+					But the real turning point was when I started learning web development on my own.
+					It was exciting, and I wanted to be really good at it.
+				</p>
+
+				<p>
+					For the past year, I've been learning a lot about web technologies like
+					JavaScript, TypeScript, NodeJS (ES6/CommonJS), HTML5, CSS3, SCSS, Angular,
+					React, Redux, Express, MongoDB, Eslint, and Prettier.
+					I used some of this cool stuff to create this website you're looking at now.
+				</p>
+
+				<p>
+					I'm thrilled to show you my projects and what I can do with programming.
+					If you have any questions or want to work together, feel free to reach out.
+					In this time I have a good expirience in TypeScript, React, SASS, NodeJS.
+				</p>
+
+				<p>
+					Thanks for visiting my website! Take care and enjoy!
+				</p>
+			</div>
+		</div>
+	</div>
+};
+
+export default HeaderContent;
---
+++ src/widgets/Header/HeaderUI/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ThemePicker from "../../../features/Theme/ThemePicker";
+import AppIcon from "../../../shared/AppIcon";
+import './style.css'
+
+const HeaderUI = () => {
+	return <div className="header-ui">
+			<div className="header-logo">
+				<AppIcon />
+				<h1 className="h1-cv">RKey CV</h1>
+			</div>
+			<div className="header-themepicker">
+				<ThemePicker />
+			</div>
+			<ul className="header-menu transition-200ms">
+				<a className="navlink" href="">
+					<li>Home</li>
+				</a>
+				<a className="navlink" href="">
+					<li>Projects</li>
+				</a>
+				<a className="navlink" href="">
+					<li>Contacts</li>
+				</a>
+			</ul>
+		</div>
+};
+
+export default HeaderUI;
