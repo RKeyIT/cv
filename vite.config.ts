a4b676296a3ebaa88e0d0014fcc73491cc46c764
Raman Krytski <rkeydevelop@gmail.com>
2023-08-06 16:59:18 +0200

    sass deleted

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import * as sass from 'sass'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
-
 		],
 })
