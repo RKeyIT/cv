1f58204f74031c42b81207c71b25461f81768144
Raman Krytski <rkeydevelop@gmail.com>
2023-08-06 16:55:02 +0200

    added sass

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import * as sass from 'sass'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
+
 		],
 })
