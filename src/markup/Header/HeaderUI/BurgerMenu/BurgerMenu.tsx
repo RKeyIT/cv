acdfbfe91bf362511e8bdc34c241c814fccf547a
Raman Krytski <rkeydevelop@gmail.com>
2023-08-08 16:34:25 +0200

    feat/refactoring: animated menu called by burger is implemented

--- src/markup/Header/HeaderUI/BurgerMenu/BurgerMenu.tsx
+++ src/markup/Header/HeaderUI/BurgerMenu/BurgerMenu.tsx
@@ -1,20 +1,25 @@
-import { useState, useRef, Ref, useEffect } from 'react';
+import { useState, useRef, Ref, useEffect, FC } from 'react';
 import './BurgerMenu.css'
 import './BurgerMenuMedia.css'
 
-const BurgerMenu = () => {
-	const [ isActive, setActive ] = useState(false);
+// Variant with refs
+// Working better than using onBlur
+const BurgerMenu: FC = () => {
+	const [ isActive, setActive ] = useState<boolean>(false);
+	const [ isDisplayNone, setisDisplayNone] = useState<boolean>(false);
+
 	const burgerBtnRef: Ref<any> = useRef(null);
 	const burgerMenuRef: Ref<any> = useRef(null);
 
-	const openMenu = () => {
+	const burgerBtnHandler = (): void => {
 		setActive(!isActive);
 	}
 
-	const isMenuFocused = (element) => {
+	const isMenuFocused = (element: EventTarget | null): boolean => {
 		return burgerBtnRef.current?.contains(element) || burgerMenuRef.current?.contains(element);
 	}
-	const handleFocusOut = (event) => {
+
+	const handleFocusOut = (event: FocusEvent): void => {
 		if (!isMenuFocused(event.relatedTarget)) {
 			setActive(false);
 		}
@@ -27,32 +32,82 @@ const BurgerMenu = () => {
 		}
 	}, [])
 
-	return <div className="header-burger-menu">
-		<button onClick={ openMenu } ref={ burgerBtnRef }
-		        className={ `transition-500ms btn-separators-container${ isActive ? '-active' : '' }` }>
-			<div className="transition-500ms btn-burger-line-1">|</div>
-			<div className="transition-500ms btn-burger-line-2">|</div>
-			<div className="transition-500ms btn-burger-line-3">|</div>
+	useEffect(() => {
+		isActive
+			? setisDisplayNone(false)
+			// TODO: Replace this with a better solution
+			// WARNING: This is a temporary solution based on animation timer in BurgerMenu.sass
+			// Rules in .active and .non-active classes
+			: setTimeout(() => setisDisplayNone(true), 500);
+	}, [ isActive ])
+
+	return <div className="header-burger">
+		<button onClick={ burgerBtnHandler } ref={ burgerBtnRef }
+		        className={ `transition-500ms burger-btn${ isActive ? '-active' : '' }` }>
+			<div className="transition-500ms burger-btn-line-1">|</div>
+			<div className="transition-500ms burger-btn-line-2">|</div>
+			<div className="transition-500ms burger-btn-line-3">|</div>
 		</button>
-		{/*{ isActive && <BurgerNav ref={burgerMenuRef} />}*/ }
-		{ isActive && <ul ref={ burgerMenuRef } className={ `transition-500ms header-burger-menu-container` }>
+		<ul ref={ burgerMenuRef }
+		    className={ `transition-500ms burger-menu ${ isActive ? 'active' : 'non-active' }`
+		      + ` ${ isDisplayNone? 'display-none' : '' }`}
+		>
 			<a className="navlink" href="#">
-				<li className="header-burger-menu-item">
+				<li className="burger-menu-item">
 					Home
 				</li>
 			</a>
 			<a className="navlink" href="#">
-				<li className="header-burger-menu-item">
+				<li className="burger-menu-item">
 					About
 				</li>
 			</a>
 			<a className="navlink" href="#">
-				<li className="header-burger-menu-item">
+				<li className="burger-menu-item">
 					Contact
 				</li>
 			</a>
-		</ul> }
+		</ul>
 	</div>
 };
+
+// Shortest variant with onBlur
+// const BurgerMenu = () => {
+// 	const [ isActive, setActive ] = useState(false);
+//
+// 	const burgerBtnHandler = () => {
+// 		setActive(!isActive);
+// 	};
+//
+// 	const handleBlur = () => {
+// 		setActive(false);
+// 	};
+//
+// 	return <div className="header-burger-menu" onBlur={ handleBlur }>
+// 		<button
+// 			onClick={ burgerBtnHandler }
+// 			className={ `transition-500ms btn-separators-container${
+// 				isActive ? '-active' : ''
+// 			}` }
+// 		>
+// 			<div className="transition-500ms burger-btn-line-1">|</div>
+// 			<div className="transition-500ms burger-btn-line-2">|</div>
+// 			<div className="transition-500ms burger-btn-line-3">|</div>
+// 		</button>
+// 		{ isActive && (
+// 			<ul className={ `transition-500ms header-burger-menu-container` }>
+// 				<a className="navlink" href="#">
+// 					<li className="header-burger-menu-item">Home</li>
+// 				</a>
+// 				<a className="navlink" href="#">
+// 					<li className="header-burger-menu-item">About</li>
+// 				</a>
+// 				<a className="navlink" href="#">
+// 					<li className="header-burger-menu-item">Contact</li>
+// 				</a>
+// 			</ul>
+// 		) }
+// 	</div>
+// };
 
 export default BurgerMenu;
