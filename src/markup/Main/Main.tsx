32f6d8930e6425bb3a982229aad81d966c42e6d5
Raman Krytski <rkeydevelop@gmail.com>
2023-08-09 21:26:20 +0200

    added Mock component for markup testing

--- src/markup/Main/Main.tsx
+++ src/markup/Main/Main.tsx
@@ -1,8 +1,10 @@
 import Technologies from "./Technologies/Technologies";
+import Mock from "./Mock/Mock";
 
 const Main = () => {
 	return <main className={`main`}>
 		<Technologies/>
+		<Mock/>
 	</main>
 };
 
