33072e4ed22d9b3717f45f52ddef7c1e2b3d68a4
Raman Krytski <rkeydevelop@gmail.com>
2023-08-09 21:10:28 +0200

    refactoring: cleared header, moon theme lvl up

--- src/markup/Header/Header.tsx
+++ src/markup/Header/Header.tsx
@@ -2,7 +2,7 @@ import './Header.css';
 import HeaderUI from "./HeaderUI/HeaderUI";
 
 const Header = () => {
-	return <header className="header-wrapper transition-1000ms">
+	return <header className="header-wrapper height transition-1000ms">
 		<div className="container">
 			<HeaderUI />
 
