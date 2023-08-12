d03437fb76b04ece6709594d5718fd518ed3291a
Raman Krytski <rkeydevelop@gmail.com>
2023-08-12 18:01:52 +0200

    fix: theme buttons is "disabalble?" >_<

--- src/markup/Header/Header.tsx
+++ src/markup/Header/Header.tsx
@@ -1,10 +1,33 @@
 import './Header.css';
 import HeaderUI from "./HeaderUI/HeaderUI";
+import { useState, useEffect } from "react";
 
 const Header = () => {
+	const imageUrls = [
+		'bg_moon.jpg',
+		'bg_noon.jpg',
+		'bg_sunrise.jpg',
+		'bg_sunset.jpg'
+	]
+	const [imagesLoaded, setImagesLoaded] = useState(false);
+
+	useEffect(() => {
+		const loadImage = (url: string) => {
+			return new Promise((resolve) => {
+				const img = new Image();
+				img.src = url;
+				img.onload = (e) => resolve(e);
+			});
+		};
+
+		Promise.all(imageUrls.map(loadImage)).then(() => {
+			setImagesLoaded(true);
+		});
+	}, []);
+
 	return <header className="header-wrapper height transition-1000ms">
 		<div className="container">
-			<HeaderUI />
+			{imagesLoaded && <HeaderUI/>}
 
 		</div>
 	</header>
--- src/markup/Main/Technologies/Technologies.tsx
+++ src/markup/Main/Technologies/Technologies.tsx
@@ -1,16 +1,12 @@
 import './style.css'
-import IconReact from "../../../shared/IconReact/IconReact";
+import Carousel from "../../../feature/Carousel/Carousel";
 
 const Technologies = () => {
 	return <div className={ `Technologies height` }>
 		<div className="container">
 			<div className="content">
 				<h2 className={`h2`}>Technologies used in project</h2>
-				<div className="carousel">
-					<div className="carousel-slide">
-						<IconReact />
-					</div>
-				</div>
+				<Carousel />
 			</div>
 		</div>
 	</div>
