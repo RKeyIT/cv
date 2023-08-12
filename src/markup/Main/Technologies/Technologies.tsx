35439123f7173947ec05dbd067d93587fb618ca0
Raman Krytski <rkeydevelop@gmail.com>
2023-08-12 08:53:33 +0200

    day theme was changed

--- src/markup/Main/Technologies/Technologies.tsx
+++ src/markup/Main/Technologies/Technologies.tsx
@@ -1,6 +1,18 @@
+import './style.css'
+import IconReact from "../../../shared/IconReact/IconReact";
+
 const Technologies = () => {
 	return <div className={ `Technologies height` }>
-
+		<div className="container">
+			<div className="content">
+				<h2 className={`h2`}>Technologies used in project</h2>
+				<div className="carousel">
+					<div className="carousel-slide">
+						<IconReact />
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 };
 
