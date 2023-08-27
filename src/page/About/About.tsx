64906190550c18d21603f9d34575ca1a9d88370f
Raman Krytski <rkeydevelop@gmail.com>
2023-08-27 17:18:45 +0200

    page: About page was filled by content and was basic styled

--- src/page/About/About.tsx
+++ src/page/About/About.tsx
@@ -1,12 +1,16 @@
 import './About.css'
 import TypingText from "./TypingText/TypingText";
+import Me from "./Me/Me";
+import Summary from "./Summary/Summary";
 
 const About = () => {
 	return <div className={ `About container` }>
-		<div className="warning">
-			<h2 className={ 'heading' }>Warning</h2>
+		<div className="warning-container">
+			<h2 className={ 'warning' }>Warning</h2>
 		</div>
 		<TypingText />
+		<Me />
+		<Summary />
 	</div>
 };
 
---
+++ src/page/About/Me/Me.tsx
@@ -0,0 +1,37 @@
+import './Me.css'
+
+const Me = () => {
+	return <div className={ `Me` }>
+			<h3 className={`heading`}>
+				Hi there! My name is <span className={`no-wrap`}>
+					Raman&nbsp;Krytski
+				</span>
+			</h3>
+			<p>
+				I am a self-taught web developer experienced with technologies such as
+				JavaScript, TypeScript, CSS and SASS/SCSS preprocessors, HTML and HTML5 semantic markup,
+				the NodeJS platform, the React library, ES6 and earlier syntaxes of JS
+				and much more interesting things :)
+			</p>
+			<p>
+				It was interesting way from basic <a href={`https://www.linkedin.com/learning/`} target={`_blank`}>
+				LinkedIn
+			</a> and <a href={`https://stepik.org/`} target={`_blank`}>
+				Stepik
+			</a> courses to more challenging paths in the <a href={`https://rs.school/`} target={`_blank`}>
+				RS School
+			</a>
+			</p>
+			<p>
+				And now I'm glad to say that I prepared to the real tasks in the real projects.
+				As you can see, this page contains custom code as well as third-party libraries.
+				I look on it like that, if I can get a good experience with writing some code
+				to solve my tasks, I can do it by my hand. But if it's something like a thousand lines
+				of code for media queries, I would prefer to use a library
+				like <a href={`https://github.com/duskload/react-device-detect`} target={`_blank`}><code>
+				react-device-detect</code></a> similar to what I did on this project.
+			</p>
+	</div>
+};
+
+export default Me;
