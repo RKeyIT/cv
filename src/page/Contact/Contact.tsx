18c1c5f02fb4742d98e07b3908f8ecc3a48b77bf
Raman Krytski <rkeydevelop@gmail.com>
2023-08-29 21:23:57 +0200

    fix: contact page added to pc version and needless copies was deleted

--- src/page/Contact/Contact.tsx
+++ src/page/Contact/Contact.tsx
@@ -1,14 +1,27 @@
-import {BrowserView, MobileView} from 'react-device-detect'
-import MobileContact from "./MobileContact/MobileContact";
+import './Contact.css'
+import CopyableText from "../../shared/CopyableText/CopyableText";
 
 const Contact = () => {
 	return <div className={ `Contact` }>
-		<BrowserView>
-			<div>Browser view</div>
-		</BrowserView>
-		<MobileView>
-			<MobileContact />
-		</MobileView>
+		<div className="container">
+			<h3>
+				Contact Information
+			</h3>
+			<p>
+				Hello there! Are you enjoying this page?
+				Feel free to get in touch if you have any offers, suggestions, or inquiries.
+			</p>
+			<p>
+				I primarily welcome connections
+				on <a className={`text-link`} href={`https://www.linkedin.com/in/rkey/`} target={'_blank'}>LinkedIn</a>,
+				but you can also reach out to me through other channels such
+				as <a className={`text-link`} href={`https://github.com/RKeyIT/`} target={'_blank'}>GitHub</a>, &nbsp;
+				<CopyableText text={`phone number`} copiedText={`+48794673466`}/>&nbsp;
+				(you can also find me on Telegram), and&nbsp;
+				<CopyableText text={`email`} copiedText={`rkeydevelop@gmail.com`}/>.
+				You'll find all the necessary contact details in the footer below.
+				Looking forward to hearing from you!
+			</p>		</div>
 	</div>
 };
 
