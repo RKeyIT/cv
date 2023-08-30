e04adf1d0cf6112cc30c434b236a914188ace735
Raman Krytski <rkeydevelop@gmail.com>
2023-08-30 22:06:30 +0200

    theme: autotheme on

--- src/app/Theme/Theme.tsx
+++ src/app/Theme/Theme.tsx
@@ -1,17 +1,18 @@
 import { useTheme } from "./ThemeContext";
+import { useEffect } from "react";
 
 const Theme = () => {
 	const {theme
-		// , setSunriseTheme, setNoonTheme, setSunsetTheme, setMoonTheme
+		, setSunriseTheme, setNoonTheme, setSunsetTheme, setMoonTheme
 	} = useTheme()
 
-	// useEffect(() => {
-	// 	const hours = new Date().getHours()
-	// 	if (hours >= 4 && hours < 10) setSunriseTheme()
-	// 	if (hours >= 10 && hours < 16) setNoonTheme()
-	// 	if (hours >= 16 && hours < 22) setSunsetTheme()
-	// 	if (hours >= 22 && hours < 4) setMoonTheme()
-	// }, [])
+	useEffect(() => {
+		const hours = new Date().getHours()
+		if (hours >= 4 && hours < 10) setSunriseTheme()
+		if (hours >= 10 && hours < 16) setNoonTheme()
+		if (hours >= 16 && hours < 22) setSunsetTheme()
+		if (hours >= 22 && hours < 4) setMoonTheme()
+	}, [])
 
 	return <style>{ theme }</style>
 }
