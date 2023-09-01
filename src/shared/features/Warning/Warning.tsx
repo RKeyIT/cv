958bfba8fbdb59c152b124c418e9f9cf3782e9e7
Raman Krytski <rkeydevelop@gmail.com>
2023-09-01 07:44:53 +0200

    Update Warning.tsx

--- src/shared/features/Warning/Warning.tsx
+++ src/shared/features/Warning/Warning.tsx
@@ -1,10 +1,11 @@
 import './Warning.css'
+import { FC } from "react";
 
 interface Props {
 	message: string
 }
 
-const Warning = ({ message }) => {
+const Warning: FC<Props> = ({ message }) => {
 	return <div className={ `Warning` }>
 		<p className={ 'message' }>{ message }</p>
 	</div>
