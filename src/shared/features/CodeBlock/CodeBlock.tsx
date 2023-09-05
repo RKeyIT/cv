eed311d05549051183e93eea3095d4bbdf017654
Raman Krytski <rkeydevelop@gmail.com>
2023-09-05 12:34:09 +0200

    buttons interactivity level up

--- src/shared/features/CodeBlock/CodeBlock.tsx
+++ src/shared/features/CodeBlock/CodeBlock.tsx
@@ -1,11 +1,12 @@
 import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { rainbow } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import { FC } from "react";
 
 interface CodeBlockProps {
 	code: string
 }
 
-const CodeBlock = ({ code }: CodeBlockProps) => {
+const CodeBlock: FC<CodeBlockProps> = ({ code }) => {
 	return <SyntaxHighlighter language={ "javascript-tsx" } style={ rainbow }>
 		{ code }
 	</SyntaxHighlighter>
